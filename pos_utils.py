4dd511387c994865fd3be8c4eb5c4050d35f185a
KivenChen <sdckivenchen@gmail.com>
2018-12-11 11:11:46 +0800

    updated

--- pos_utils.py
+++ pos_utils.py
@@ -1,3 +1,5 @@
+from math import sin, cos, tan, sqrt, radians as r
+
 '''
 this file provides the utilities for tracking robot and boxes locations
 the **core** module will import this file and create *boxes* and *pos* for use
@@ -8,8 +10,8 @@ so that the core tech of coordinating will not be open-sourced
 class Position:
 	x = 0
 	def __init__(self):
-		self.x = 0
-		self.y = 0
+		self.x = 0.0
+		self.y = 0.0
 		self.d = 0
 		self.accurate = True
 	
@@ -18,15 +20,37 @@ class Position:
 		# d_delta: the change of direction clockwise
 		# dpm: the displacement x
 		# continuous: handle f() and b() separately
-		pass
+		if continuous：
+			pass
+		self.d += d_delta
+		dx += dpm * sin(r(d))
+		dy += dpm * cos(r(d))
+		
+	def from(self, pos):
+		# output distance
+		return math.sqrt((self.x-pos.x)**2+(self.y - pos.y)**2)
+
+class Box:
+	def __init__(self, pos, ok=True):
+		self.pos = pos
+		self.go = ok
 
 
 class Boxes(list):
 	def __init__(self):
 		pass
 	
-	def add(self, pos, is_bonus):
-		Boxes.append([pos.x, pos.y, pos.d])
+	def add(self, pos, go):
+		self.append(Box(pos, go))
 		
 	
-	
+	def nearest(self, robo_pos):
+		min = 0
+		mindist = 99999
+		for i, box in enumerate(self):
+			dist = robo_pos.from(box.pos)
+			if dist < mindist and box.go: 
+				mindist = dist
+				min = box
+		return min
+		
